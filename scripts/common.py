ce1652bb5bea025fc747ae5b21fddf5b5c9ffc43
Jack C. <40553992+JHCelaya@users.noreply.github.com>
2025-09-05 17:19:51 -0700

    Models tested
    all models being compared:
    
    google/pegasus-pubmed
    google/pegasus-arxiv
    facebook/bart-large-cc
    allenai/led-based-16384

--- scripts/common.py
+++ scripts/common.py
@@ -16,3 +16,11 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 # Reuse app utilities
 from app.pdf_io import extract_text_by_page
 from app.sections import split_into_sections, stitch_sections
+
+
+DEFAULT_MODELS = [
+    "google/pegasus-pubmed",
+    "google/pegasus-arxiv",
+    "facebook/bart-large-cnn",
+    "allenai/led-base-16384",
+]
